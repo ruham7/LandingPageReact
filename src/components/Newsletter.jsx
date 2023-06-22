c7a9e3d561cc5462ec8ed6e16a30b28813af833f
ruhama <ruhama.demeke@gmail.com>
2023-06-22 16:13:07 +0300

    Newsletter component

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Newsletter = () => {
+  return (
+    <>
+      <div className="text-white py-16 px-6 w-full">
+        <div className="bg-black mx-auto max-w-[1240px] grid lg:grid-cols-2 ">
+          <div>
+            <h1 className="mr-2 font-bold text-xl sm:text-2xl md:text-3xl">
+              Want tips & tricks to optimize your flow?
+            </h1>
+            <p className="mt-2">
+              Sign up to our newsletter and stay up to date.
+            </p>
+          </div>
+          <div>
+            <div className="py-6 grid md:grid-cols-2 ">
+              <input
+                type="text"
+                className="rounded py-2 "
+                placeholder=" Enter your email"
+              />
+              <button className="bg-[#00df9a] w-[150px] rounded-md font-medium mx-auto py-2 mt-4 md:mt-0 text-black">
+                {" "}
+                Notify me
+              </button>
+            </div>
+            <p className=" my-6">
+              We care about the protection of your data. Read our{" "}
+              <span className="text-[#00df9a] underline">Privacy policy.</span>{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Newsletter;
